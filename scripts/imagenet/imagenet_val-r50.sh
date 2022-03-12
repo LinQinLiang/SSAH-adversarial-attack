45448faafa88f5552709dcde7f66fa0fab23297f
Qinliang Lin <45589968+LinQinLiang@users.noreply.github.com>
2022-03-12 21:49:07 +0800

    Update imagenet_val-r50.sh

--- scripts/imagenet/imagenet_val-r50.sh
+++ scripts/imagenet/imagenet_val-r50.sh
@@ -1,1 +1,13 @@
-
+#ImageNet1M
+python  ../../main.py \
+        --classifier='resnet50' \
+        --dataset='imagenet_val'  \
+        --bs=256 \
+        --max_epoch=1 \
+        --wavelet='haar' \
+        --num_iteration=200 \
+        --learning_rate=0.0001 \
+        --m=0.2 \
+        --alpha=1.0 \
+        --beta=0.1\
+        --seed=18
