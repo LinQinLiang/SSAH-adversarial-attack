837e55673c37a31db07bdc9272a049c1b698ced0
Qinliang Lin <45589968+LinQinLiang@users.noreply.github.com>
2022-03-14 23:22:28 +0800

    Update imagenet_val-r50.sh

--- scripts/imagenet/imagenet_val-r50.sh
+++ scripts/imagenet/imagenet_val-r50.sh
@@ -10,4 +10,17 @@ python  ../../main.py \
         --m=0.2 \
         --alpha=1.0 \
         --beta=0.1\
-        --seed=18
+        --seed=8
+
+python  ../../main.py \
+        --classifier='resnet50' \
+        --dataset='imagenet_val'  \
+        --bs=256 \
+        --max_epoch=1 \
+        --wavelet='haar' \
+        --num_iteration=200 \
+        --learning_rate=0.0001 \
+        --m=0.2 \
+        --alpha=1.0 \
+        --beta=0.0\
+        --seed=8
