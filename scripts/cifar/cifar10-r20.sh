463cc04656a08e0cf893cc3b8a3460b61812b79c
Qinliang Lin <45589968+LinQinLiang@users.noreply.github.com>
2022-03-14 23:21:23 +0800

    Update cifar10-r20.sh

--- scripts/cifar/cifar10-r20.sh
+++ scripts/cifar/cifar10-r20.sh
@@ -2,7 +2,7 @@
 python  ../../main.py \
         --classifier='resnet20' \
         --dataset='cifar10'  \
-        --bs=10000 \
+        --bs=5000 \
         --max_epoch=1 \
         --wavelet='haar' \
         --num_iteration=150 \
@@ -10,4 +10,20 @@ python  ../../main.py \
         --m=0.2 \
         --alpha=1 \
         --beta=0.1\
-        --seed=18
+        --seed=8\
+        --workers=32
+
+
+python  ../../main.py \
+        --classifier='resnet20' \
+        --dataset='cifar10'  \
+        --bs=5000 \
+        --max_epoch=1 \
+        --wavelet='haar' \
+        --num_iteration=150 \
+        --learning_rate=0.001 \
+        --m=0.2 \
+        --alpha=1 \
+        --beta=0.0\
+        --seed=8\
+        --workers=32
