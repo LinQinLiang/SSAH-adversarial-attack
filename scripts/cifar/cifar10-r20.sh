87abea5b511345ef23911321572655ee4f136e00
Leo <45589968+LinQinLiang@users.noreply.github.com>
2022-03-16 19:53:34 +0800

    Update cifar10-r20.sh

--- scripts/cifar/cifar10-r20.sh
+++ scripts/cifar/cifar10-r20.sh
@@ -1,17 +1,18 @@
 #CIFAR10 TEST 10K
-python  ../../main.py \
-        --classifier='resnet20' \
-        --dataset='cifar10'  \
-        --bs=5000 \
-        --max_epoch=1 \
-        --wavelet='haar' \
-        --num_iteration=150 \
-        --learning_rate=0.001 \
-        --m=0.2 \
-        --alpha=1 \
-        --beta=0.1\
-        --seed=8\
-        --workers=32
+#python  ../../main.py \
+#        --classifier='resnet20' \
+#        --dataset='cifar10'  \
+#        --bs=5000 \
+#        --max_epoch=1 \
+#        --wavelet='haar' \
+#        --num_iteration=150 \
+#        --learning_rate=0.001 \
+#        --m=0.2 \
+#        --alpha=1 \
+#        --beta=0.1\
+#        --seed=8\
+#        --workers=32\
+#        --test_fid=True
 
 
 python  ../../main.py \
@@ -26,4 +27,5 @@ python  ../../main.py \
         --alpha=1 \
         --beta=0.0\
         --seed=8\
-        --workers=32
+        --workers=32\
+        --test_fid=True
