23c11eb6ab72f5c1a57bb3feffdf7238de47a2e3
lingjivoo <35754447+lingjivoo@users.noreply.github.com>
2022-04-04 20:40:06 +0800

    Add files via upload

--- scripts/cifar/cifar100-r20.sh
+++ scripts/cifar/cifar100-r20.sh
@@ -1,31 +1,29 @@
-#CIFAR10 TEST 10K
+#CIFAR100 TEST 10K
 #python  ../../main.py \
 #        --classifier='resnet20' \
-#        --dataset='cifar10'  \
+#        --dataset='cifar100'  \
 #        --bs=5000 \
 #        --max_epoch=1 \
 #        --wavelet='haar' \
 #        --num_iteration=150 \
-#        --learning_rate=0.001 \
+#        --learning_rate=0.0015 \
 #        --m=0.2 \
 #        --alpha=1 \
-#        --beta=0.1\
+#        --beta=0.01\
 #        --seed=8\
-#        --workers=32\
 #        --test_fid=True
 
 
 python  main.py \
         --classifier='resnet20' \
-        --dataset='cifar10'  \
+        --dataset='cifar100'  \
         --bs=5000 \
         --max-epoch=1 \
         --wavelet='haar' \
         --num-iteration=150 \
-        --learning-rate=0.001 \
+        --learning-rate=0.0015 \
         --m=0.2 \
         --alpha=1 \
         --beta=0.0\
         --seed=8\
-        --workers=32\
         --test-fid
