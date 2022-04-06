06f31506843ec0b9c0da7322f08e8257be71238d
lingjivoo <35754447+lingjivoo@users.noreply.github.com>
2022-04-06 12:32:15 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def parse_arg():
     parser.add_argument('--learning-rate', type=float, default=0.001, help='LEARNING RATE')
     parser.add_argument('--m', type=float, default=0.2, help='MARGIN')
     parser.add_argument('--alpha', type=float, default=1.0, help='HYPER PARAMETER FOR ADV COST')
-    parser.add_argument('--beta', type=float, default=0.1, help='HYPER PARAMETER FOR LOW FREQUENCY CONSTRAINT')
+    parser.add_argument('--lambda-lf', type=float, default=0.1, help='HYPER PARAMETER FOR LOW FREQUENCY CONSTRAINT')
     parser.add_argument('--outdir', type=str, default='result', help='dir to save the attack examples')
 
     args = parser.parse_args()
@@ -77,13 +77,16 @@ elif opt.dataset == 'cifar100':
     data, num_images = load_cifar100(opt)
 else:
     data, num_images = load_imagenet_val(opt)
-print("Attack Benign Image of {} dataset({} images) with perturb mode: {} :".format(
+print("Attack Benign Image of {} dataset ({} images) with perturb mode: {} :".format(
     opt.dataset, num_images, opt.perturb_mode
 ))
 
 total_img = 0
 att_suc_img = 0
 PerD = PerceptualDistance(opt)
+img_list_for_fid = None
+adv_img_list_for_fid = None
+fid = 0
 
 att = SSAH(model=classifier,
            num_iteration=opt.num_iteration,
@@ -93,7 +96,7 @@ att = SSAH(model=classifier,
            dataset=opt.dataset,
            m=opt.m,
            alpha=opt.alpha,
-           beta=opt.beta,
+           lambda_lf=opt.lambda_lf,
            wave=opt.wavelet)
 
 for batch, (inputs, targets) in enumerate(data):
@@ -113,31 +116,40 @@ for batch, (inputs, targets) in enumerate(data):
     adv = adv[att_suc_id]
     inputs = inputs[att_suc_id]
 
-    l2, l_inf, low_fre, ssim = PerD.cal_perceptual_distances(inputs,adv)
-    PerD.update(l2, l_inf, low_fre, ssim, adv.size(0))
+    l2, l_inf, low_fre, ssim, CIEDE2000 = PerD.cal_perceptual_distances(inputs, adv)
+    PerD.update(l2, l_inf, low_fre, ssim, CIEDE2000, adv.size(0))
 
     # Test the fid Value：we save the ori and adv img into .png profile and test them use fid
     # save the 5k imgs to test the fid
     if opt.test_fid:
-        if batch == 0:
-            benign_img = os.path.join(opt.outdir, opt.dataset + '/' + 'benign-SSA-H3521/')
-            adv_img = os.path.join(opt.outdir, opt.dataset + '/' + 'adv-SSAH-3521/')
+        benign_img = os.path.join(opt.outdir, opt.dataset + '/' + 'benign-3521/')
+        adv_img = os.path.join(opt.outdir, opt.dataset + '/' + 'adv-SSAH-3521/')
+        if img_list_for_fid is None and adv_img_list_for_fid is None:
+            img_list_for_fid = []
+            adv_img_list_for_fid = []
             if not os.path.exists(benign_img):
                 os.makedirs(benign_img)
             if not os.path.exists(adv_img):
                 os.makedirs(adv_img)
-            for id in range(adv.shape[0]):
-                vutils.save_image(inputs[id].detach(),
-                                  '%s/%5d.png' % (benign_img, id),
+        img_list_for_fid.append(inputs.detach())
+        adv_img_list_for_fid.append(adv.detach())
+
+        if att_suc_img >= 5000:
+            opt.test_fid = False
+            img_list_for_fid = torch.cat(img_list_for_fid,dim=0)
+            adv_img_list_for_fid = torch.cat(adv_img_list_for_fid,dim=0)
+            for id in range(5000):
+                vutils.save_image(img_list_for_fid[id].detach(),
+                                  '%s/%05d.png' % (benign_img, id),
                                   normalize=True,
                                   )
-                vutils.save_image(adv[id].detach(),
-                                  '%s/%5d.png' % (adv_img, id),
+                vutils.save_image(adv_img_list_for_fid[id].detach(),
+                                  '%s/%05d.png' % (adv_img, id),
                                   normalize=True,
                                   )
             fid = return_fid(benign_img, adv_img)
-    else:
-        fid = 0
+            del img_list_for_fid
+            del adv_img_list_for_fid
 
     print("Evaluating Adversarial images of {} dataset({} images) with perturb mode:{} :".format(
         opt.dataset, total_img, opt.perturb_mode))
@@ -146,12 +158,14 @@ for batch, (inputs, targets) in enumerate(data):
           "L2: {:.2f} "
           "L_inf: {:.2f} "
           "SSIM: {:.2f} "
+          "CIEDE2000: {:.2f} "
           "Low Frequency: {:.2f} "
           "FID Value: {:.2f}".format(batch,
                                      100.0 * att_suc_img / total_img,
                                      PerD.l2_avg,
                                      PerD.l_inf_avg,
                                      PerD.ssim_avg,
+                                     PerD.CIEDE2000_avg,
                                      PerD.LF_avg,
                                      fid))
 
