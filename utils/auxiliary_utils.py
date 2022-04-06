e82bf82c48aaa29c2453ab06155e8ca6ae059a81
lingjivoo <35754447+lingjivoo@users.noreply.github.com>
2022-04-06 13:06:38 +0800

    Add files via upload

--- utils/auxiliary_utils.py
+++ utils/auxiliary_utils.py
@@ -2,7 +2,8 @@ from torchvision import datasets, transforms
 import numpy as np
 import random
 import torch
-
+import os
+import logging
 
 def setup_seed(seed):
     torch.manual_seed(seed)
@@ -39,11 +40,11 @@ def attack_success(targets, pred):
 
 
 def load_cifar10(opt):
-    path = 'dataset/cifar10/'
+    path = os.path.join(opt.dataset_root, 'cifar10/')
     transform = transforms.Compose([transforms.ToTensor()])
     dataset = datasets.CIFAR10(root=path,
                                train=False,
-                               transform=transform)
+                               transform=transform,download=True)
     dataloader = torch.utils.data.DataLoader(dataset=dataset,
                                              batch_size=opt.bs,
                                              shuffle=True,
@@ -53,11 +54,11 @@ def load_cifar10(opt):
 
 
 def load_cifar100(opt):
-    path = 'dataset/cifar-100-python/'
+    path = os.path.join(opt.dataset_root, 'cifar-100-python/')
     transform = transforms.Compose([transforms.ToTensor()])
     dataset = datasets.CIFAR100(root=path,
                                 train=False,
-                                transform=transform)
+                                transform=transform,download=True)
     dataloader = torch.utils.data.DataLoader(dataset=dataset,
                                              batch_size=opt.bs,
                                              shuffle=True,
@@ -67,7 +68,7 @@ def load_cifar100(opt):
 
 
 def load_imagenet_val(opt):
-    path = 'dataset/ILSVRC2012/val/'
+    path = os.path.join(opt.dataset_root, 'ILSVRC2012/val/')
     transform = transforms.Compose([transforms.Resize(256),
                                     transforms.CenterCrop(224),
                                     transforms.ToTensor()])
@@ -80,3 +81,62 @@ def load_imagenet_val(opt):
                                              pin_memory=True
                                              )
     return dataloader, len(dataset)
+
+
+def print_conf(opt):
+    """Print and save options
+    It will print both current options and default values(if different).
+    It will save options into a text file / [checkpoints_dir] / opt.txt
+    """
+    message = ''
+    message += '----------------- Options ---------------\n'
+    for k, v in sorted(vars(opt).items()):
+        comment = ''
+        # default = self.parser.get_default(k)
+        # if v != default:
+        #     comment = '\t[default: %s]' % str(default)
+        message += '{:>25}: {:<30}{}\n'.format(str(k), str(v), comment)
+    message += '----------------- End -------------------'
+    return message
+
+
+def set_logger(opt):
+    """Set the logger to log info in terminal and file `log_path`.
+
+    In general, it is useful to have a logger so that every output to the terminal is saved
+    in a permanent file. Here we save it to `model_dir/train.log`.
+
+    Example:
+    ```
+    logging.info("Starting training...")
+    ```
+
+    Args:
+        log_path: (string) where to log
+    """
+
+    if 'loglevel' in opt:
+        loglevel = eval('logging.'+loglevel)
+    else:
+        loglevel = logging.INFO
+
+    outname = 'attack.log'
+    outdir = opt.outdir
+    log_path = os.path.join(outdir,outname)
+
+    logger = logging.getLogger()
+    logger.setLevel(loglevel)
+
+    if not logger.handlers:
+        # Logging to a file
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+        logger.addHandler(file_handler)
+
+        # Logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(stream_handler)
+
+    logging.info(print_conf(opt))
+    logging.info('writting logs to file {}'.format(log_path))
