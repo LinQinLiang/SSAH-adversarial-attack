f7c24d11023c31e5cc2b4adcd9c27839b0808bc4
Leo <45589968+LinQinLiang@users.noreply.github.com>
2022-04-10 22:14:43 +0800

    Update imagenet_val-r50.sh

--- scripts/imagenet/imagenet_val-r50.sh
+++ scripts/imagenet/imagenet_val-r50.sh
@@ -8,9 +8,9 @@ python  main.py \
         --wavelet='haar' \
         --num-iteration=200 \
         --learning-rate=0.0001 \
-        --m=0.0 \
+        --m=0.2 \
         --alpha=1.0 \
-        --lambda-lf=0.5\
+        --lambda-lf=0.1\
         --seed=8
 
 #SSA
